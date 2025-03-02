b87b4e9a1895e5c25407f3d090fa397871909f62
Rajini W. <rajini.wijayanayake@city.ac.uk>
2025-03-02 21:04:25 +0000

    date ideas

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 date_ideas = [
     {
-        "At home" : ""
+        "At home" : ["DIY Wine & Paint Night", "Indoor Picnic", "Coof-Off Challenge", "Home Spa Night", "Board Game Marathon",
+        "Build A Blanket Fort", "Karaoke Night", "DIY Pizza Night", "Stargazing Indoors"]
+    },
+    {
+        "Indoor" : ["Escape Room", "Museum or Art Gallery", "Bowling", "Indoor Rock Climbing", "Comedy Show", 
+        "Arcade or Barcade", "Theater or Live Performance", "Indoor Trampoline Park", "Wine or Beer Tasting"]
+    },
+    {
+        "Outdoor" : ["Hiking Adventures", "Beach Day", "Drive-in Movie", "Farmers' Market", "Kayaking or Canoeing", 
+        "Botanical Garden Visit", "Sunset Picnic", "Outdoor Sports", "Zoo or Aquarium", "Camping Trip"]
     }
 ]
